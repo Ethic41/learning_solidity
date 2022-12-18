02093acb6f27c7639977c1e1d48668b1a90193dd
Dahir Muhammad Dahir <dahirmuhammad3@gmail.com>
2022-12-18 16:54:00 +0100

    vulnerable timelock vault

---
+++ vuln_timelock.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+contract TimeLock {
+    
+    mapping(address => uint) public balances;
+    mapping(address => uint) public lockTime;
+
+    function deposit() public payable {
+        balances[msg.sender] += msg.value;
+        lockTime[msg.sender] = block.timestamp + 1 weeks;
+    }
+
+    function increaseLockTime(uint secondsToIncrease) public {
+        lockTime[msg.sender] += secondsToIncrease;
+    }
+
+    function withdraw() public {
+        require(balances[msg.sender] > 0);
+        require(block.timestamp > lockTime[msg.sender]);
+        balances[msg.sender] = 0;
+        payable(msg.sender).transfer(balances[msg.sender]);
+    }
+
+}
