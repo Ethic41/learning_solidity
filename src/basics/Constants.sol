4dec7e3080f6bf750fe09c7b76c0ddc889d91b23
ethic41 <dahirmuhammad3@gmail.com>
2024-08-05 21:10:06 +0100

    chore: Add Constants contract in Solidity

---
+++ src/basics/Constants.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+// -=-<[ Bismillahirrahmanirrahim ]>-=-
+// -*- coding: utf-8 -*-
+// @Date    : 2024-08-05 20:49:39
+// @Author  : Dahir Muhammad Dahir (dahirmuhammad3@gmail.com)
+pragma solidity ^0.8.26;
+
+contract Constants {
+    // coding conventions to uppercase
+    address public constant MY_ADDRESS = 0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
+    uint256 public constant MY_UINT = 123;
+}
+
