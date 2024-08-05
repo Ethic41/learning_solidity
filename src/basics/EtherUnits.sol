bcbae28f3573aff45ebba356d7a2cf13702d8e48
ethic41 <dahirmuhammad3@gmail.com>
2024-08-05 22:26:41 +0100

    feat: Add EtherUnits contract in Solidity
    The commit adds a new Solidity contract, EtherUnits, to the project. The EtherUnits contract includes variables and boolean checks for different ether units such as wei, gwei, and ether. It demonstrates the conversion and comparison of these units in Solidity.

---
+++ src/basics/EtherUnits.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+// -=-<[ Bismillahirrahmanirrahim ]>-=-
+// -*- coding: utf-8 -*-
+// @Date    : 2024-08-05 22:10:06
+// @Author  : Dahir Muhammad Dahir (dahirmuhammad3@gmail.com)
+pragma solidity ^0.8.26;
+
+contract EtherUnits {
+    uint256 public oneWei = 1 wei;
+    // 1 wei is equal to 1
+    bool public isOneWei = (oneWei == 1);
+
+    uint256 public oneGwei = 1 gwei;
+    // 1 gwei is equal to 10^9 wei
+    bool public isOneGwei = (oneGwei == 1e9);
+
+    uint256 public oneEther = 1 ether;
+    // 1 ether is equal to 10^18 wei
+    bool public isOneEther = (oneEther == 1e18);
+}
+
