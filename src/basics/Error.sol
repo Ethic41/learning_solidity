da74a7576be5db198f8aea96254c3e7426277de0
ethic41 <dahirmuhammad3@gmail.com>
2024-08-07 15:02:36 +0100

    feat: Add Error contract in Solidity
    The commit adds a new Solidity contract, Error, to the project. The Error contract includes functions to demonstrate the usage of require, revert, and assert statements in Solidity. It also includes a custom error for handling insufficient balance.

---
+++ src/basics/Error.sol
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: MIT
+// -=-<[ Bismillahirrahmanirrahim ]>-=-
+// -*- coding: utf-8 -*-
+// @Date    : 2024-08-07 14:11:38
+// @Author  : Dahir Muhammad Dahir (dahirmuhammad3@gmail.com)
+pragma solidity ^0.8.26;
+
+contract Error {
+    function testRequire(uint256 _i) public pure {
+        require (_i > 10, "Input must be greater than 10");
+    }
+
+    function testRevert(uint256 _i) public pure {
+        if (_i <= 10) {
+            revert("Input must be greater than 10");
+        }
+    }
+
+    uint256 public num;
+
+    function testAssert() public view {
+        assert(num == 0);
+    }
+
+    // custom error
+    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
+
+    function testCustomError(uint256 _withdrawAmount) public view {
+        uint256 bal = address(this).balance;
+        if (bal < _withdrawAmount) {
+            revert InsufficientBalance({
+                balance: bal,
+                withdrawAmount: _withdrawAmount
+            });
+        }
+    }
+}
+
+contract Account {
+    uint256 public balance;
+    uint256 public constant MAX_UINT = type(uint256).max;
+
+    function deposit(uint256 _amount) public {
+        uint256 oldBalance = balance;
+        uint256 newBalance = balance + _amount;
+
+        // balance + _amount does not overflow if balance + _amount >= balance
+        require(newBalance >= oldBalance, "Overflow");
+
+        balance = newBalance;
+
+        assert(balance >= oldBalance);
+    }
+
+    function withdraw(uint256 _amount) public {
+        uint256 oldBalance = balance;
+
+        // balance - _amount does not underflow if balance >= _amount
+        require(balance >= _amount, "Underflow");
+
+        if (balance < _amount) {
+            revert("Underflow");
+        }
+
+        balance -= _amount;
+
+        assert(balance <= oldBalance);
+    }
+}
